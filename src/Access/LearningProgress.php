35594a6b415dc0424284e13a4c2a424d64c264e5
Florian Wyss <fw@studer-raimann.ch>
2019-02-12 15:07:53 +0100

    Using LearningProgressPie from CustomInputGUIs

--- src/Access/LearningProgress.php
+++ src/Access/LearningProgress.php
@@ -2,10 +2,7 @@
 
 namespace srag\Plugins\SrLearningProgressPDBlock\Access;
 
-use ilLearningProgressBaseGUI;
 use ilLPObjSettings;
-use ilLPStatus;
-use ilObjectLP;
 use ilObjUser;
 use ilSrLearningProgressPDBlockPlugin;
 use srag\DIC\SrLearningProgressPDBlock\DICTrait;
@@ -23,12 +20,6 @@ class LearningProgress {
 	use SrLearningProgressPDBlockTrait;
 	use DICTrait;
 	const PLUGIN_CLASS_NAME = ilSrLearningProgressPDBlockPlugin::class;
-	const LP_STATUS_COLOR = [
-		ilLPStatus::LP_STATUS_NOT_ATTEMPTED_NUM => "#DDDDDD",
-		ilLPStatus::LP_STATUS_IN_PROGRESS_NUM => "#F6D842",
-		ilLPStatus::LP_STATUS_COMPLETED_NUM => "#BDCF32",
-		ilLPStatus::LP_STATUS_FAILED => "#B06060"
-	];
 	/**
 	 * @var self[]
 	 */
@@ -78,32 +69,5 @@ class LearningProgress {
 		} else {
 			return false;
 		}
-	}
-
-
-	/**
-	 * @param int $obj_id
-	 *
-	 * @return int
-	 */
-	public function getStatus(int $obj_id): int {
-		// Avoid exit
-		if (ilObjectLP::getInstance($obj_id)->getCurrentMode() != ilLPObjSettings::LP_MODE_UNDEFINED) {
-			$status = intval(ilLPStatus::_lookupStatus($obj_id, $this->user->getId()));
-		} else {
-			$status = ilLPStatus::LP_STATUS_NOT_ATTEMPTED_NUM;
-		}
-
-		return $status;
-	}
-
-
-	/**
-	 * @param int $status
-	 *
-	 * @return string
-	 */
-	public function getText(int $status): string {
-		return ilLearningProgressBaseGUI::_getStatusText($status);
 	}
 }
