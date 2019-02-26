39e6e91abd44fddee6d415c8b30a4f96e1118e6f
Florian Wyss <fw@studer-raimann.ch>
2019-02-26 08:39:29 +0100

    Fix

--- src/Block/BaseBlock.php
+++ src/Block/BaseBlock.php
@@ -4,8 +4,10 @@ namespace srag\Plugins\SrLearningProgressPDBlock\Block;
 
 use ilBlockGUI;
 use ilSrLearningProgressPDBlockPlugin;
+use ilTemplateException;
 use srag\CustomInputGUIs\SrLearningProgressPDBlock\CustomInputGUIsTrait;
 use srag\DIC\SrLearningProgressPDBlock\DICTrait;
+use srag\DIC\SrLearningProgressPDBlock\Exception\DICException;
 use srag\Plugins\SrLearningProgressPDBlock\Utils\SrLearningProgressPDBlockTrait;
 
 /**
@@ -61,7 +63,8 @@ abstract class BaseBlock extends ilBlockGUI {
 
 
 	/**
-	 *
+	 * @throws DICException
+	 * @throws ilTemplateException
 	 */
 	public function fillDataSection()/*: void*/ {
 		$obj_ids = array_filter($this->obj_ids, function (int $obj_id): bool {
@@ -109,7 +112,7 @@ abstract class BaseBlock extends ilBlockGUI {
 
 
 	/**
-	 *
+	 * @throws DICException
 	 */
 	protected abstract function initTitle()/*: void*/
 	;
