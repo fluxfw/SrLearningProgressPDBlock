2b07372f2a2a4c002426c4903f535e3a21714490
sragfw <fw@studer-raimann.ch>
2019-11-26 09:34:11 +0100

    fix_wrong_db_table

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -2,3 +2,15 @@
 <?php
 \srag\Plugins\SrLearningProgressPDBlock\Config\Config::updateDB();
 ?>
+<#2>
+<?php
+if (\srag\DIC\SrLearningProgressPDBlock\DICStatic::dic()->database()->tableExists(\srag\Plugins\SrLearningProgressPDBlock\Config\Config::TABLE_NAME_WRONG)) {
+    \srag\DIC\SrLearningProgressPDBlock\DICStatic::dic()->database()->dropTable(\srag\Plugins\SrLearningProgressPDBlock\Config\Config::TABLE_NAME, false);
+
+    \srag\DIC\SrLearningProgressPDBlock\DICStatic::dic()
+        ->database()
+        ->renameTable(\srag\Plugins\SrLearningProgressPDBlock\Config\Config::TABLE_NAME_WRONG, \srag\Plugins\SrLearningProgressPDBlock\Config\Config::TABLE_NAME);
+} else {
+    \srag\Plugins\SrLearningProgressPDBlock\Config\Config::updateDB();
+}
+?>
